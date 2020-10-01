35763017d7a52f09d2374e0d1a4fa2b940817f5e
Vy Dao <vynguyendao@gmail.com>
2020-10-01 22:27:07 +0700

    Added: adding form

--- cmd/web/templates.go
+++ cmd/web/templates.go
@@ -1,9 +1,64 @@
 package main
 
 import (
+    "html/template"
+    "net/url"
+    "path/filepath"
+    "time"
+
+    "vy.dao/snippetbox/pkg/forms"
     "vy.dao/snippetbox/pkg/models"
 )
 
 type templateData struct {
-    Snippet    *models.Snippet
+    CurrentYear   int
+    Form          *forms.Form
+    Snippet       *models.Snippet
+    Snippets      []*models.Snippet
+}
+
+func humanDate(t time.Time) string {
+    if t.IsZero() {
+        return ""
+    }
+
+    return t.UTC().Format("02 Jan 2006 at 15:04")
+}
+
+var functions = template.FuncMap{
+    "humanDate": humanDate,
+}
+
+func newTemplateCache(dir string) (map[string]*template.Template, error) {
+    cache := map[string]*template.Template{}
+
+    pages, err := filepath.Glob(filepath.Join(dir, "*.page.tmpl"))
+
+    if err != nil {
+      return nil, err
+    }
+
+    for _, page := range pages {
+      name := filepath.Base(page)
+
+      template, err := template.New(name).Funcs(functions).ParseFiles(page)
+
+      if err != nil {
+        return nil, err
+      }
+
+      template, err = template.ParseGlob(filepath.Join(dir, "*.layout.tmpl"))
+
+      if err != nil {
+        return nil, err
+      }
+
+      template, err = template.ParseGlob(filepath.Join(dir, "*.partial.tmpl"))
+      if err != nil {
+        return nil, err
+
+      }
+      cache[name] = template
+    }
+    return cache, nil
 }
